d7795c9481fe8fce21fb2acf1299073960be938f
Starky <mcwyzards@gmail.com>
2021-03-16 17:44:43 -0400

    Added void damage to damageable entities and items when in darkness

--- src/main/java/com/Theeef/me/Shadebound.java
+++ src/main/java/com/Theeef/me/Shadebound.java
@@ -1,17 +1,31 @@
 package com.Theeef.me;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Shadebound extends JavaPlugin {
+public class Shadebound extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Shadebound Enabled");
+        getServer().getPluginManager().registerEvents(this, this);
+
+        VoidDamage.voidDamageLoop();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    @EventHandler
+    public void dropItem(PlayerDropItemEvent event) {
+        event.getPlayer().sendMessage("Damage: " + (event.getItemDrop().getItemStack().getType().getMaxDurability() - ((Damageable) event.getItemDrop().getItemStack().getItemMeta()).getDamage()));
     }
 }
--- src/main/java/com/Theeef/me/VoidDamage.java
+++ src/main/java/com/Theeef/me/VoidDamage.java
@@ -1,4 +1,51 @@
 package com.Theeef.me;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class VoidDamage {
+
+    public static Shadebound plugin = Shadebound.getPlugin(Shadebound.class);
+
+    public static void voidDamageLoop() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (World world : Bukkit.getWorlds()) {
+                    for (Entity entity : world.getEntities()) {
+                        int light = entity.getLocation().getBlock().getLightLevel();
+                        double damage = (7 - light) / 7.0 * 8;
+
+                        if (damage > 0) {
+                            if (entity instanceof Damageable) {
+                                ((Damageable) entity).damage(damage);
+                                entity.setLastDamageCause(new EntityDamageEvent(entity, EntityDamageEvent.DamageCause.VOID, damage));
+                            } else if (entity instanceof Item) {
+                                ItemStack itemstack = ((Item) entity).getItemStack();
+                                ItemMeta meta = itemstack.getItemMeta();
+                                org.bukkit.inventory.meta.Damageable damageable = (org.bukkit.inventory.meta.Damageable) meta;
+                                int dura = itemstack.getType().getMaxDurability() - damageable.getDamage();
+
+                                if (dura - (int) (damage * 5) <= 0)
+                                    entity.remove();
+                                else {
+                                    damageable.setDamage(dura - (int) (damage * 5));
+                                    itemstack.setItemMeta(meta);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 0, 10);
+    }
 }
